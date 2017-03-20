bdea6da8b4469b59388ac0f81979e11894d39f91
Mrutyunjay swain <mrutyunjay.aei@gmail.com>
2017-03-20 16:21:55 -0500

    dd

--- src/Demo1.java
+++ src/Demo1.java
@@ -1,13 +1,8 @@
 
 public class Demo1 {
-<<<<<<< HEAD
 
-	public static void main(String args) {
-		
-	}
-=======
 public static void main(String[] args) {
 	
 }
->>>>>>> branch 'master' of https://github.com/butu187/My-Git.git
+
 }
